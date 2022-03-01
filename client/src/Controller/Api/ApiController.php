b099308ee6caca3f7d6c491cdf63dd65a0618f6b
azbytniewski <azbytniewski@cxindex.com>
2022-03-01 17:47:42 +0100

    Added security guards, login form + user fixture

--- client/src/Controller/Api/ApiController.php
+++ client/src/Controller/Api/ApiController.php
@@ -8,6 +8,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @IsGranted("IS_AUTHENTICATED_FULLY")
+ */
+
 class ApiController extends AbstractController
 {
     private $itemService;
@@ -19,6 +25,7 @@ class ApiController extends AbstractController
 
     public function __construct(ItemService $itemService)
     {
+        
         $this->itemService = $itemService;
     }
 
